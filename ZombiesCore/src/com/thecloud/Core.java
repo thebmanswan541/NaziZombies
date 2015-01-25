0d98e71c0022f7ddba1c4575afec3c02dbcbdf07
thebmanswan541 <theeternitynetwork@gmail.com>
2015-01-25 18:48:41 -0500

    Update test.

--- ZombiesCore/src/com/thecloud/Core.java
+++ ZombiesCore/src/com/thecloud/Core.java
@@ -1,11 +1,13 @@
 package com.thecloud;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Core extends JavaPlugin {
+public class Core extends JavaPlugin implements Listener{
 
     public void onEnable() {
-
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
 }
