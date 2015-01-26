fee4e0c446ed194c6c08d303e420fba76f664655
thebmanswan541 <theeternitynetwork@gmail.com>
2015-01-26 09:01:59 -0500

    General update.

--- ZombiesCore/src/com/thecloud/Structure/ChatUtilities.java
+++ ZombiesCore/src/com/thecloud/Structure/ChatUtilities.java
@@ -3,9 +3,6 @@ package com.thecloud.Structure;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
-/**
- * Created by Mitchell on 1/25/2015.
- */
 public class ChatUtilities {
 
     public static void broadcast(String message) {
