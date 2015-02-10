9b58c9c4e7a47e01a64f6a3b9a41a3fa1e854955
thebmanswan541 <theeternitynetwork@gmail.com>
2015-02-10 08:39:05 -0500

    Big update.

---
+++ ZombiesCore/src/com/thecloud/Listeners/CreateDoor.java
@@ -0,0 +1,48 @@
+package com.thecloud.Listeners;
+
+import com.thecloud.Structure.Door;
+import com.thecloud.Structure.DoorManager;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.ArrayList;
+
+public class CreateDoor implements Listener{
+
+    private String s;
+    ArrayList<Player> door;
+
+    @EventHandler
+    public void onSign(SignChangeEvent e) {
+        Player p = e.getPlayer();
+        for (int i = 0; i < e.getLines().length; i++) {
+            if (e.getLine(i).toLowerCase().contains("door")) {
+                door.add(p);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInt(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if (door.contains(p)) {
+            if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
+                Block b = e.getClickedBlock();
+                if (b.getState() instanceof Sign) {
+                    Sign s = (Sign) b.getState();
+                    DoorManager.getInstance().addDoor(new Door(s.getLine(0), s.getWorld().getName()+":"+s.getLocation().getX()+":"+s.getLocation().getY()+":"+s.getLocation().getZ(), Integer.parseInt(s.getLine(1))));
+                    Door d = DoorManager.getInstance().getDoor(s.getLine(0));
+                    s.setLine(0, null); s.setLine(1, ChatColor.RED+""+d.getCost());
+                }
+            }
+        }
+    }
+}
--- ZombiesCore/src/com/thecloud/Structure/Door.java
+++ ZombiesCore/src/com/thecloud/Structure/Door.java
@@ -9,11 +9,19 @@ public class Door implements ConfigurationSerializable{
 
     private String name;
     private String location;
+    private int cost;
     private boolean unlocked;
+
+    public Door(String name, String location, int cost) {
+        this.name = name;
+        this.location = location;
+        this.cost = cost;
+    }
 
     public Door(String name, Map<String, Object> map) {
         this.name = name;
         this.location = (String) map.get("location");
+        this.cost = (Integer) map.get("cost");
         this.unlocked = (Boolean) map.get("unlocked");
     }
 
@@ -37,9 +45,19 @@ public class Door implements ConfigurationSerializable{
         this.location = location;
     }
 
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
     public Map<String, Object> serialize() {
         HashMap<String, Object> map = new HashMap<String, Object>();
         map.put("location", location);
+        map.put("cost", cost);
         map.put("unlocked", unlocked);
         return map;
     }
